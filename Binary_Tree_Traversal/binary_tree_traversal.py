796ccc58a43252c8d8cb4dfec3ff793805a0ef74
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-23 14:30:26 +0300

    Added in-order traversal for "binary_tree_traversal.py"

--- Binary_Tree_Traversal/binary_tree_traversal.py
+++ Binary_Tree_Traversal/binary_tree_traversal.py
@@ -30,7 +30,33 @@ def pre_order(node):
 # In-order traversal
 def in_order(node):
     ''''''
-    return []
+    if node is None:
+        return []
+    res = []
+
+    def rec(node, path = None, res = None):
+        if res is None:
+            res = []
+        if path is None:
+            path = []
+
+        if node.left is None:
+            res.append(node)
+
+        if node and node.left and node.left not in res:
+            path.append(node)
+            rec(node.left, path, res)
+            if node not in res:
+                res.append(node)
+
+        if node and node.right and node.right not in res:
+            path.append(node)
+            rec(node.right, path, res)
+            if node not in res:
+                res.append(node)
+
+    rec(node, [], res)
+    return [i.data for i in res]
 
 # Post-order traversal
 def post_order(node):
