21b318dc79a4d7fb1c915e0a9d57d4acb5a1a1f3
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-23 21:58:12 +0300

    Made a solution of "delete_node_in_a_bst.py"

--- Delete_Node_in_a_BST/delete_node_in_a_bst.py
+++ Delete_Node_in_a_BST/delete_node_in_a_bst.py
@@ -13,3 +13,24 @@ class Solution:
     ''''''
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         ''''''
+        if root is None:
+            return
+        if root.val == key:
+            if root.left is None:
+                return root.right
+
+            if root.right is None:
+                return root.left
+
+            cur_node = root.right
+            while cur_node.left:
+                cur_node = cur_node.left
+            root.val = cur_node.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+
+        return root
