214f3450f58b2f1dcc8c14b49a4b3b0de9899bd7
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-23 22:00:41 +0300

    Added docstrings and typehinting for "delete_node_in_a_bst.py"

--- Delete_Node_in_a_BST/delete_node_in_a_bst.py
+++ Delete_Node_in_a_BST/delete_node_in_a_bst.py
@@ -1,18 +1,19 @@
-''''''
+'''A solution of a problem "Delete Node in a BST"'''
 from typing import Optional
 
 # Definition for a binary tree node.
 class TreeNode:
-    ''''''
-    def __init__(self, val=0, left=None, right=None):
+    '''Represents a node in a binary tree'''
+    def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, \
+right: Optional['TreeNode'] = None):
         self.val = val
         self.left = left
         self.right = right
 
 class Solution:
-    ''''''
+    '''Provides solution to problem of deliting special node from binary search tree'''
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        ''''''
+        ''' Delete the node with the given key from the binary search tree'''
         if root is None:
             return
         if root.val == key:
