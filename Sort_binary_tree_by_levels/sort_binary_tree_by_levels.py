4dca1e410bbe13eedcfbe3584bfd73ec123ec765
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-04-23 20:13:33 +0300

    Made a soltion of "sort_binary_tree_by_levels.py"

--- Sort_binary_tree_by_levels/sort_binary_tree_by_levels.py
+++ Sort_binary_tree_by_levels/sort_binary_tree_by_levels.py
@@ -1,5 +1,21 @@
 ''''''
+from collections import deque
 
 def tree_by_levels(node):
     ''''''
-    return
+    if node is None:
+        return []
+    res = [node.value]
+    deq = deque()
+    if node.left:
+        deq.append(node.left)
+    if node.right:
+        deq.append(node.right)
+    while deq:
+        node = deq.popleft()
+        res.append(node.value)
+        if node.left:
+            deq.append(node.left)
+        if node.right:
+            deq.append(node.right)
+    return res
